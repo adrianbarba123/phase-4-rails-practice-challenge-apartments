2be4f8d4c9231ee7fc2b6452d2936636d99194f5
Adrian Barba <adrianbarba123@gmail.com>
2023-02-09 20:53:44 -0800

    commit tree 787f6337a55f8b74ec4f1f34e443ae81cb3c4276

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :leases, only: [:post, :destroy]
+  resources :tenants
+  resources :apartments #, only: [:index, :show, :patch, :post, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
 end
